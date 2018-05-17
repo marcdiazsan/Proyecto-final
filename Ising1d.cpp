0c17a580e804d019eec17ab71655209ff1e443eb
Slackware Live User <live@darkstar.example.net>
2018-05-16 20:53:17 -0700

    Programa Modelo de Ising 1d

---
+++ Ising1d.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<random>
+#include<omp.h>
+#include<cmath>
+#include<vector>
+const double J=1;
+const double k=1;
+const int N=500;
+void fill( std::vector<double> &s);
+void change();
+double energy();
+double total_energy();
+double magnetization();
+double fluctuation();
+double specific_heat();
+int main(int argc, char *argv)
+{
+  double T=10000;
+  std::vector<double> s(N);
+  void fill(std::vector<double> &s);
+  
+  
+
+}
+void fill(std::vector<double> &s)
+{
+  int seed=5;
+  std::mt19937 gen(seed);
+  std::uniform_real_distribution<> dis(0,1);
+  
+  for (int i=0; i<N; i++)
+    {
+      double n=dis(gen);
+      if(n<=0.5)
+	s[i]=-1;
+      else if(n>0.5)
+	s[i]=1;
+    }
+}
